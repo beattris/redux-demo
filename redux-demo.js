695be839b9cf84e0127e26ccc6425a38227de694
Beattris <beatriceebirim@gmail.com>
2022-02-25 16:50:38 +0100

    created action types and dispatched the actions

--- redux-demo.js
+++ redux-demo.js
@@ -1,16 +1,33 @@
 const redux = require("redux");
 
-const counterReducer = (state = {counter: 0}, action) => {
-  return {
-    counter: state.counter + 1,
+// CREATING A REDUCER FUNCTION
+const counterReducer = (state = { counter: 0 }, action) => {
+  // CHECKING THE ACTION TYPE AND DISPACTCHING THE ACTION ACCORDINGLY
+  if (action.type === 'INCREMENT'){
+      return {
+          counter: state.counter + 1
+      };
   };
+  if (action.type === 'DECREMENT'){
+      return {
+          counter: state.counter - 1
+      };
+  }
+  return state;
 };
 
+// CREATING A STORE TO HOLD THE REDUCER DETAILS
 const store = redux.createStore(counterReducer);
 
+// A COMPONENT THAT IS SUBSCRIBING TO THE STORE
 const counterSubscriber = () => {
-    const latestState = store.getState();
-    console.log(latestState);
-}
+  const latestState = store.getState();
+  console.log(latestState);
+};
 
+// SUBSCRIBES THE COMPONENT TO THE STORE
 store.subscribe(counterSubscriber);
+
+// DISPATCHING THE ACTION
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "DECREMENT" });
